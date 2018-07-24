d67d068b073f23b09b9d6ebefab2ea3020e33005
tylergaugler16 <tylergaugler16@gmail.com>
2018-07-23 21:59:09 -0400

    adding assets

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+		//options here
+		// autoScrolling:true,
+		// scroll: true,
+    scrollOverflow: true,
+    licenseKey: "OPEN-SOURCE-GPLV3-LICENSE",
+	});
+
+	//methods
+	// $.fn.fullpage.setAllowScrolling(false);
+});
