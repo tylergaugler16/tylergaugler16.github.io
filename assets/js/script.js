772dd7ca5df98f68f2b8d51a7bd530e9a6fec37a
Tyler Gaugler <tylergaugler16@gmail.com>
2018-10-20 15:54:38 -0400

    more styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,14 @@
-const $backgroundYellow = "#FFD200";
-const $backgroundGray = "#e1e5ee";
-const $backgroundRed = "#D33F49";
-const $backgroundGreen = "#73BFB8";
+const $backgroundYellow = 'rgba(255, 210, 0, 0.9)' //"#FFD200";
+const $backgroundGray   = "#e1e5ee";
+const $backgroundRed    = "#D33F49";
+const $backgroundGreen  = "#73BFB8";
+const $backgroundBlue   = 'rgba(0, 65, 139, 0.9)' // "#00418b";
+
+
+
+const $backgroundDarkBlue = 'rgba(2, 47, 64, 0.9)' // "#022F40";
+const $backgroundOrange = 'rgba(240, 135, 0, 0.9)' // "#F08700";
+const $backgroundLightBlue = 'rgba(56, 174, 204, 0.9)' //"#38AECC";
 
 $(document).ready(function() {
   $("#fullpage").fullpage({
@@ -9,14 +16,19 @@ $(document).ready(function() {
     // autoScrolling:true,
     // scroll: true,
     sectionsColor: [
-      "#044389",
-      $backgroundGreen,
-      "#ffd200",
-      "#7CAFC4",
-      "#73BFB8"
+      $backgroundBlue,
+      $backgroundOrange,
+      $backgroundDarkBlue,
+      $backgroundYellow,
+      $backgroundLightBlue,
     ],
+    scrolloverflow: true,
+    anchors: ['home', 'skills', 'projects', 'experience', 'contact'],
     scrollingSpeed: 800,
     licenseKey: "OPEN-SOURCE-GPLV3-LICENSE",
+    menu: '#menu',
+    navigationTooltips: ['Page 1', 'Page 2', 'Any text!'],
+    showActiveTooltip: true,
     onLeave: function(origin, destination, direction) {
       if (origin.index === 0) {
         setTimeout(
@@ -28,11 +40,11 @@ $(document).ready(function() {
         setTimeout(() => $(".first-section").addClass("hovered animate"), 800);
       }
 
-      if(origin.index === 2 ){
+      if(origin.index === 3 ){
           // setTimeout(() => $(".experience-section").removeClass("animate"), 800);
           $(".experience-section").removeClass("animate")
       }
-      if(destination.index === 2){
+      if(destination.index === 3){
         setTimeout(() => $(".experience-section").addClass("animate"), 200);
       }
 
@@ -165,3 +177,235 @@ $(document).ready(function() {
     });
   }
 });
+
+
+
+
+
+
+
+
+
+var treeData = [
+  {
+    "name": "Projects",
+    "parent": "null",
+    "children": [
+      {
+        "name": "YouNote",
+        "parent": "Projects",
+        "project_id": "younote",
+        "children": [
+          {
+            "name": "Tech Stack",
+            "parent": "YouNote",
+            "project_id": "younote",
+            "children": [
+              {
+                "name": "Ruby On Rails",
+                "parent": "Tech Stack",
+                "project_id": "younote",
+              },
+              {
+                "name": "HTML/CSS",
+                "parent": "Tech Stack",
+                "project_id": "younote",
+              }
+            ]
+          },
+          {
+            "name": "Description",
+            "parent": "YouNote",
+            "project_id": "younote",
+          }
+        ]
+      },
+      {
+        "name": "Attendance/Registration Mangement System",
+        "parent": "Projects",
+        "children": [
+          {
+            "name": "Tech Stack",
+            "parent": "Attendance/Registration Mangement System",
+            "children": [
+              {
+                "name": "Node.js",
+                "parent": "Tech Stack"
+              },
+              {
+                "name": "Express.js",
+                "parent": "Tech Stack"
+              }
+            ]
+          },
+          {
+            "name": "Description",
+            "parent": "Attendance/Registration Mangement System"
+          }
+        ]
+      }
+    ]
+  }
+];
+
+
+$(document).ready(function(){
+  // ************** Generate the tree diagram	 *****************
+  var margin = {top: 20, right: 120, bottom: 20, left: 120},
+  	width = 960 - margin.right - margin.left,
+  	height = 500 - margin.top - margin.bottom;
+
+  var i = 0,
+  	duration = 750,
+  	root;
+
+  var tree = d3.layout.tree()
+  	.size([height, width]);
+
+  var diagonal = d3.svg.diagonal()
+  	.projection(function(d) { return [d.y, d.x]; });
+
+  var svg = d3.select(".projects-section .fp-tableCell").append("svg")
+  	.attr("width", width + margin.right + margin.left)
+  	.attr("height", height + margin.top + margin.bottom)
+    .append("g")
+  	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  root = treeData[0];
+  root.x0 = height / 2;
+  root.y0 = 0;
+
+  update(root);
+
+  // setTimeout(() => , 1000)
+  // dft(root);
+  for(var i = 0; i< root.children.length; i++){
+    click(root.children[i]);
+  }
+  // click(root);
+
+
+  d3.select(self.frameElement).style("height", "500px");
+  function update(source) {
+
+
+    // Compute the new tree layout.
+    var nodes = tree.nodes(root).reverse(),
+  	  links = tree.links(nodes);
+
+
+    // Normalize for fixed-depth.
+    nodes.forEach(function(d) { d.y = d.depth * 180; });
+
+    // Update the nodes…
+    var node = svg.selectAll("g.node")
+  	  .data(nodes, function(d) { return d.id || (d.id = ++i); });
+
+
+    // Enter any new nodes at the parent's previous position.
+    var nodeEnter = node.enter().append("g")
+  	  .attr("class", "node")
+  	  .attr("transform", function(d) { return "translate(" + source.y0 + "," + source.x0 + ")"; })
+  	  .on("click", click);
+
+    nodeEnter.append("circle")
+  	  .attr("r", 1e-6)
+  	  .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
+
+    nodeEnter.append("text")
+  	  .attr("x", function(d) { return d.children || d._children ? -25 : 25; })
+  	  .attr("dy", ".45em")
+  	  .attr("text-anchor", function(d) { return d.children || d._children ? "end" : "start"; })
+  	  .text(function(d) { return d.name; })
+  	  .style("fill-opacity", 1e-6);
+
+    // Transition nodes to their new position.
+    var nodeUpdate = node.transition()
+  	  .duration(duration)
+  	  .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; });
+
+    nodeUpdate.select("circle")
+  	  .attr("r", 10)
+  	  .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
+
+    nodeUpdate.select("text")
+  	  .style("fill-opacity", 1);
+
+    // Transition exiting nodes to the parent's new position.
+    var nodeExit = node.exit().transition()
+  	  .duration(duration)
+  	  .attr("transform", function(d) { return "translate(" + source.y + "," + source.x + ")"; })
+  	  .remove();
+
+    nodeExit.select("circle")
+  	  .attr("r", 1e-6);
+
+    nodeExit.select("text")
+  	  .style("fill-opacity", 1e-6);
+
+    // Update the links…
+    var link = svg.selectAll("path.link")
+  	  .data(links, function(d) { return d.target.id; });
+
+    // Enter any new links at the parent's previous position.
+    link.enter().insert("path", "g")
+  	  .attr("class", "link")
+  	  .attr("d", function(d) {
+  		var o = {x: source.x0, y: source.y0};
+  		return diagonal({source: o, target: o});
+  	  });
+
+    // Transition links to their new position.
+    link.transition()
+  	  .duration(duration)
+  	  .attr("d", diagonal);
+
+    // Transition exiting nodes to the parent's new position.
+    link.exit().transition()
+  	  .duration(duration)
+  	  .attr("d", function(d) {
+  		var o = {x: source.x, y: source.y};
+  		return diagonal({source: o, target: o});
+  	  })
+  	  .remove();
+
+    // Stash the old positions for transition.
+    nodes.forEach(function(d) {
+  	d.x0 = d.x;
+  	d.y0 = d.y;
+    });
+  }
+
+  // Toggle children on click.
+  function click(d) {
+    console.log(d);
+    // change the displayed picture based on d.project_id. defined in the json
+    if (d.children) {
+  	d._children = d.children;
+  	d.children = null;
+    } else {
+  	d.children = d._children;
+  	d._children = null;
+    }
+    update(d);
+  }
+
+  function dft(root){
+    var stack=[];
+    var animX=0;
+    stack.push(root);
+    while(stack.length!==0){
+      var element = stack.pop();
+      if(!element.name === "Projects"){
+        click(element);
+      }
+      // visitElement(element,animX);
+      animX=animX+1;
+      if(element.children!==undefined){
+        for(var i=0; i<element.children.length; i++){
+          stack.push(element.children[element.children.length-i-1]);
+        }
+      }
+    }
+  }
+})
