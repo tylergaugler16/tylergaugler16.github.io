1d104643edc44b58fbf18e4e1ab1e841ab161456
Tyler Gaugler <tylergaugler16@gmail.com>
2018-10-23 20:08:32 -0400

    add parallex backgorunds

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,13 +24,12 @@ $(document).ready(function() {
     ],
     scrolloverflow: true,
     parallax: true,
-    percentage: 100,
+    parallaxOptions: {type: 'cover', percentage: 100},
     anchors: ['home', 'skills', 'projects', 'experience', 'contact'],
     scrollingSpeed: 800,
     licenseKey: "OPEN-SOURCE-GPLV3-LICENSE",
     menu: '#menu',
-    navigationTooltips: ['Page 1', 'Page 2', 'Any text!'],
-    showActiveTooltip: true,
+
     onLeave: function(origin, destination, direction) {
       if (origin.index === 0) {
         setTimeout(
@@ -56,95 +55,6 @@ $(document).ready(function() {
   // $.fn.fullpage.setAllowScrolling(false);
 });
 
-// $(document).ready(function() {
-//   $(".first-section.draw").addClass("hovered animate");
-//
-//   var triggers = $("ul.triggers li");
-//   var images = $("ul.projects li");
-//   var lastElem = $("ul.triggers li").length - 1;
-//   var view = $(".view ul.projects");
-//   var imgWidth = images.width();
-//   var descriptions = $(".project-description");
-//   var target;
-//   console.log(imgWidth);
-//   triggers.first().addClass("selected");
-//   descriptions.first().addClass("selected");
-//   view.css("width", imgWidth * (lastElem + 1) + "px");
-//
-//   function sliderResponse(target) {
-//     view
-//       .stop(true, false)
-//       .animate({ left: "-" + imgWidth * target + "px" }, 500);
-//     triggers
-//       .removeClass("selected")
-//       .eq(target)
-//       .addClass("selected");
-//     descriptions
-//       .removeClass("selected")
-//       .eq(target)
-//       .addClass("selected");
-//   }
-//
-//   triggers.click(function() {
-//     if (!$(this).hasClass("selected")) {
-//       target = $(this).index();
-//       sliderResponse(target);
-//       // resetTiming();
-//     }
-//   });
-//
-//   $(".next").click(function() {
-//     target = $("ul.triggers li.selected").index();
-//     target === lastElem ? (target = 0) : (target = target + 1);
-//     sliderResponse(target);
-//     // resetTiming();
-//   });
-//   $(".prev").click(function() {
-//     target = $("ul.triggers li.selected").index();
-//     lastElem = triggers.length - 1;
-//     target === 0 ? (target = lastElem) : (target = target - 1);
-//     sliderResponse(target);
-//     // resetTiming();
-//   });
-//
-//   const $filters = $(".project-filter [data-filter]");
-//   // $boxes = $('.skills-flexbox [data-category]');
-//
-//   $(document).on("click", "#project1, #project2, #project3", function() {
-//     console.log("here");
-//     var $this = $(this);
-//     $filters.removeClass("active"); // removes active from the projects
-//     $this.addClass("active"); // adds active to the current projects
-//
-//     var $projectFilter = $this.attr("data-filter");
-//     highlightSkillsOfProject($projectFilter);
-//   });
-//
-//   const highlightSkillsOfProject = function(projectName) {
-//     console.log(projectName);
-//     const $skills = $(".skills-flexbox .skill");
-//     if (projectName == "all") {
-//       $skills
-//         .removeClass("is-animated")
-//         .fadeOut()
-//         .promise()
-//         .done(function() {
-//           $skills.addClass("is-animated").fadeIn();
-//         });
-//     } else {
-//       $skills
-//         .removeClass("is-animated")
-//         .fadeOut()
-//         .promise()
-//         .done(function() {
-//           $skills
-//             .filter(".skill[ " + projectName + "='true']")
-//             .addClass("is-animated")
-//             .fadeIn();
-//         });
-//     }
-//   };
-// });
 
 
 $(document).on('click', '.healthie-trigger', function(){
